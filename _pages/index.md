0cf13c5928fb71f21888a9823309b60680962b82
AutumnAurelium <88015631+AutumnAurelium@users.noreply.github.com>
2024-09-09 13:37:38 -0400

    add disco elysium portraits

--- _pages/index.md
+++ _pages/index.md
@@ -7,6 +7,8 @@ permalink: /
 
 Hi, I'm aurelium. This website is currently empty.
 
+[Trivial - Success!!!Are you ever actually going to use this bit for a real post, my liege?](#portrait-drama)
+
 ```python
 a = math.sin(15)
 print(f"Hello, world! {a}")
---
+++ assets/js/global.js
@@ -0,0 +1,52 @@
+let portraits = document.querySelectorAll('a[href^="#portrait-"]');
+
+function toTitleCase(str) {
+    return str.toLowerCase().replace(/(?:^|\s)\w/g, function(match) {
+      return match.toUpperCase();
+    });
+}
+
+let colormap = {
+    "drama": "portrait-intellect",
+    "logic": "portrait-intellect",
+    "rhetoric": "portrait-intellect",
+    "encyclopedia": "portrait-intellect",
+    "conceptualization": "portrait-psyche",
+    "volition": "portrait-psyche",
+    "inland-empire": "portrait-psyche",
+    "empathy": "portrait-psyche",
+    "authority": "portrait-physique",
+    "endurance": "portrait-physique",
+    "physical-instrument": "portrait-physique",
+    "electrochemistry": "portrait-physique",
+    "suggestion": "portrait-motorics",
+    "composure": "portrait-motorics",
+    "hand-eye-coordination": "portrait-motorics",
+    "perception": "portrait-motorics",
+    "reaction-speed": "portrait-motorics",
+    "savoir-faire": "portrait-motorics",
+    "interfacing": "portrait-motorics",
+    "esprit-de-corps": "portrait-motorics"
+}
+
+portraits.forEach(function(portrait) {
+    let text = portrait.innerHTML;
+
+    let check = "";
+
+    if(text.includes("!!!")) {
+        let split = text.split("!!!");
+        text = split[1];
+        check = "[" + split[0] + "]";
+    }
+
+    let character = portrait.href.split('#portrait-')[1];
+    let characterName = toTitleCase(character.split("-").join(" "));
+    let image = `<image class="portrait-image" src="/assets/portraits/${character}.jpg" alt="portrait of ${characterName}">`
+    
+    let skillName = `<span class=${colormap[character]}>${characterName} </span>`
+    let skillCheck = `<span class="portrait-skillcheck">${check}</span>`
+    let dialog = `<span>${skillName}${skillCheck} <span class="portrait-dialog">— ${text}</span></span>`
+    
+    portrait.outerHTML = "<blockquote class='portrait'>" + image + dialog + "</blockquote>"
+});
